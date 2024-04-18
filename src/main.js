352d52a6697c3769008c24c78ccd370a677d3cb3
Denis Dezest <denis.dezest@gmail.com>
2024-04-18 16:18:09 +0200

    📦 Add bootstrap and sass for custom theme configuration

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+/// -----------------------------------------------------------------------------
+/// OPTION 1: import all of Bootstrap's JS
+// import * as bootstrap from 'bootstrap'
+/// -----------------------------------------------------------------------------
+
+/// -----------------------------------------------------------------------------
+/// OPTION 2: only import needed Bootstrap components
+// import { Tooltip, Toast, Popover, Collapse.. } from 'bootstrap';
+/// -----------------------------------------------------------------------------
+
 console.log('Hello World!');
