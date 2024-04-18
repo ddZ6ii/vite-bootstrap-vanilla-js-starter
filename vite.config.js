f5ce17e16c387568d85cbc093873674bedd54c17
Denis Dezest <denis.dezest@gmail.com>
2024-04-18 09:53:41 +0200

    🔧 Add vite config file

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Define chrome as default browser for the dev server.
+const opsys = process.platform;
+// windows
+if (opsys === 'win32') process.env.BROWSER = 'chrome';
+// macOS
+if (opsys === 'darwin') process.env.BROWSER = '/Applications/Google Chrome.app';
+
+export default defineConfig({
+  root: resolve(__dirname, './src'),
+  publicDir: resolve(__dirname, './public'),
+  build: {
+    emptyOutDir: true,
+    outDir: resolve(__dirname, './dist'),
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@assets': resolve(__dirname, './assets'),
+      '@bootstrap': resolve(__dirname, './node_modules/bootstrap/scss'),
+      '@js': resolve(__dirname, './src/js'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  plugins: [],
+});
